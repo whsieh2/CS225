9497ccca4d668f07cd958a7a40a10f15529a8622
wchsieh2 <wchsieh2@0bbf1d51-50cb-4d16-abda-e800905df8e7>
2013-09-09 03:04:50 +0000

    lab_debug submission

--- lab_debug/main.cpp
+++ lab_debug/main.cpp
@@ -1,5 +1,8 @@
 #include <cstdlib>
 #include "png.h"
+#include <iostream>
+
+using namespace std;
 
 // sets up the output image
 PNG * setupOutput(int w, int h);
@@ -10,37 +13,43 @@ RGBAPixel * myFavoriteColor(int intensity);
 int main()
 {
 	// Load in.png
-	PNG * original;
+	PNG * original = new PNG();
 
 	original->readFromFile("in.png");
 	int width  = original->width();
 	int height = original->height();
-
 	// Create out.png
-	PNG * output;
-	setupOutput(width, height);	
-
+	PNG * output = setupOutput(width, height);	
+	
 	// Loud our favorite color to color the outline
 	RGBAPixel * myPixel = myFavoriteColor(192);
+		
 
 	// Go over the whole image, and if a pixel differs from that to its upper
 	// left, color it my favorite color in the output
-	for (int y = 1; 0 < y < height; y++)
+	for (int y = 1; y < height; y++)
 	{
-		for (int x = 1; 0 < x < width; x++)
+		for (int x = 1;  x < width; x++)
 		{
+		
 			// Calculate the pixel difference
 			RGBAPixel * prev = (*original)(x-1, y-1);
 			RGBAPixel * curr = (*original)(x  , y  );
 			int diff = abs(curr->red   - prev->red  ) +
 					   abs(curr->green - prev->green) +
 					   abs(curr->blue  - prev->blue );
-
 			// If the pixel is an edge pixel,
 			// color the output pixel with my favorite color
 			RGBAPixel * currOutPixel = (*output)(x,y);
 			if (diff > 100)
-				currOutPixel = myPixel;
+			{
+				*currOutPixel = *myPixel; //addr of currOutPixel is now the addr of myPixel
+				//(*currOutPixel).myPixel;
+				
+
+// Dereference and actually replace the values of currOutPixel with myPixel
+				
+			}	
 		}
 	}
 	
@@ -66,10 +75,10 @@ PNG * setupOutput(int w, int h)
 // Returns my favorite color
 RGBAPixel * myFavoriteColor(int intensity)
 {
-	RGBAPixel color;
-	color.red   = 0;
-	color.green = intensity/2;
-	color.blue  = intensity;
+	RGBAPixel * color = new RGBAPixel;
+	color->red   = 0;
+	color->green = intensity/2;
+	color->blue  = intensity;
 	&color;
 }
 
