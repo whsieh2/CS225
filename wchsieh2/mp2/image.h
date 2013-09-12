badc5c3619ec6f0aa390d4357de14848403081e0
wchsieh2 <wchsieh2@0bbf1d51-50cb-4d16-abda-e800905df8e7>
2013-09-12 03:21:42 +0000

    fixed my mp2

--- wchsieh2/mp2/image.h
+++ wchsieh2/mp2/image.h
@@ -5,8 +5,8 @@
 class Image : public PNG
 {
 	public:  //constructor 
-   		void flipleft();
-		void adjustbrightness(int r, int g, int b);
-		void invertcolors();
+   		void flipleft();  //flips the image around the y axis.
+		void adjustbrightness(int r, int g, int b);  //adds to the red, blue, and green pixel values.
+		void invertcolors(); //makes each rgb pixel to 255 minus its original value 
 };
 #endif
